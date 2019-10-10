4ed6747bd90c24630e8f11ccd8b6d72750c49b27
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-10-10 18:48:26 +0200

    add recursive split

--- res/src/split.c
+++ res/src/split.c
@@ -31,3 +31,37 @@ ud_arr_char_a                      *ud_stra_split(ud_arr_char_a *str, char *sep)
     ud_str_split_free_len(begin);
     return new_arr;
 }
+
+static ud_arr   *ud_stra_split_rec(ud_arr *str, char **floor_sep)
+{
+    if (!floor_sep || !*floor_sep) return NULL;
+    char *sep = *floor_sep;
+    ++floor_sep;
+    ud_arr *splitted = ud_stra_split(str, sep);
+    if (splitted->type_s == 0 && *floor_sep)
+    {
+        size_t len = splitted->len;
+        ud_arr **val = (ud_arr**)splitted->val;
+        ud_arr *new_val;
+        while (len-- > 0)
+        {
+            new_val = ud_stra_split_rec(*val, floor_sep);
+            ud_arr_free(*val);
+            *val++ = new_val;
+        }
+    }
+    return splitted;
+}
+
+ud_arr          *ud_stra_vsplit_call(ud_arr *str, size_t args_len, ...)
+{
+    va_list args;
+    va_start(args, args_len);
+    char *sep[args_len + 1];
+    char **sep_tmp = sep;
+    while (args_len-- > 0) *sep_tmp++ = (char*)va_arg(args, char*);
+    *sep_tmp = NULL;
+    ud_arr *parsed = ud_stra_split_rec(str, sep);
+    va_end(args);
+    return parsed;
+}
