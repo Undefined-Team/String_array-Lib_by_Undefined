6278a2e30550a9b12e9cd8cae896ddc51cbc470c
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-10-16 16:06:08 +0200

    can compil version

--- res/src/join.c
+++ res/src/join.c
@@ -1,13 +1,13 @@
 #include "ud_string_array.h"
 
-ud_arr    *ud_stra_join_ctr(ud_arr *str, char *sep, ud_bool need_free)
-{
-    if (!str) return ud_stra_new("");  
-    size_t sep_len = ud_str_len(sep);
-    size_t total_len = 0;
-    ud_arr *str_tmp = str;
-    size_t str_nbr = str->len
-    size_t strs_len[str_nbr];
-    size_t *strs_len_tmp = strs_len;
+// ud_arr    *ud_stra_join_ctr(ud_arr *str, char *sep, ud_bool need_free)
+// {
+//     if (!str) return ud_stra_new("");  
+//     size_t sep_len = ud_str_len(sep);
+//     size_t total_len = 0;
+//     ud_arr *str_tmp = str;
+//     size_t str_nbr = str->len
+//     size_t strs_len[str_nbr];
+//     size_t *strs_len_tmp = strs_len;
 
-}
+// }
