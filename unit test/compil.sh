72d22e9ce6aa7c6ef468d9d73bc61f4d14b8534b
sbedene <sylvainbedene@gmail.com>
2019-10-29 18:44:39 +0100

    update compil.sh

--- unit test/compil.sh
+++ unit test/compil.sh
@@ -3,5 +3,5 @@
 if [ "$1" != "quick" ]; then
 ../setup.sh $1 $2
 fi
-gcc test.c $lib -lud_math -lud_array -lud_utils -lud_string -lud_string_array -lud_memory -o executable
+gcc test.c -lud_string_array -o executable
 ./executable
